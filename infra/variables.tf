e495c2cc5f96363074235c1fb12e23066f5d9280
dawright22 <dr@hashicorp.com>
2024-07-03 10:09:19 +1000

    vault

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,15 +2,3 @@ variable "region" {
   type    = string
   default = "eastus"
 }
-
-//vault address
-variable "vault_addr" {
-  type    = string
-  default = "http://localhost:8200"
-}
-
-//vault token
-variable "vault_token" {
-  type    = string
-  default = "s.1234567890"
-}
